3513e0d05693c0a09eb6e13be42693d5de61aafc
jin07150 <jin07150@gmail.com>
2020-03-29 19:55:51 +0900

    change Label control's property with two buttons 'show' and 'hide'.
     . for binding in view(*.xaml), viewmodel's variable should be get/set(SetProperty(ref .., value))

---
+++ PrismMVVM_wpfControlPopertyChangeWithButton/Models/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismMVVM_wpfControlPopertyChangeWithButton.Models
+{
+    class TestModel : BindableBase
+    {
+        private Visibility _MessageVisibilty;
+        public Visibility MessageVisibilityJJW
+        {
+            get { return _MessageVisibilty; }
+            set { SetProperty(ref _MessageVisibilty, value); }
+        }
+    }
+}
